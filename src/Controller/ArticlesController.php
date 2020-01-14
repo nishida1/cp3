d8296b246ae94afd801a28aed26b636902074fc2
nishida1 <42903695+nishida1@users.noreply.github.com>
2020-01-14 15:32:34 +0900

    ArticlesController

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class ArticlesController extends AppController
+{
+	public $autoRender = false;
+
+	public function index()
+	{
+		echo "test";
+	}
+
+}
