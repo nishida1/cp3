6a37bbccd29524d42ce649bab6f8b42050c77a20
nishida1 <42903695+nishida1@users.noreply.github.com>
2020-01-15 11:42:36 +0900

    pagenate active

--- config/paginator-templates.php
+++ config/paginator-templates.php
@@ -5,5 +5,5 @@ return [
     'prevActive' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
     'prevDisabled' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
     'number' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
-    'current' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+    'current' => '<li class="page-item active"><a class="page-link" href="{{url}}">{{text}}</a></li>',
 ];
